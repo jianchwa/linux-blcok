a255ae61a3cec7fbacffe8226a5c5755223c9213
Jianchao Wang <jianchao.w.wang@oracle.com>
2019-01-09 17:41:09 +0800

    simple_cache: phase 2
    Setup the cache mpping
    
    Signed-off-by: Jianchao Wang <jianchao.w.wang@oracle.com>

--- drivers/md/simple_cache.c
+++ drivers/md/simple_cache.c
@@ -6,6 +6,7 @@
 #include <linux/sysfs.h>
 #include <asm/local.h>
 #include <linux/blk-mq.h>
+#include <linux/log2.h>
 
 #include "simple_cache.h"
 
@@ -25,6 +26,8 @@ static LIST_HEAD(sc_all_backing_dev);
  * Protect list sc_all_backing_dev
  */
 static struct mutex sc_all_backing_dev_lock;
+
+static sc_entry_t *sc_get_mapping_entry(struct sc_backing_dev *scbd, sector_t sec);
 
 static void sc_start_io_acct(struct sc_io *sio)
 {
@@ -144,6 +147,162 @@ static blk_qc_t sc_passthrough(struct sc_io *sio)
 	return submit_bio(clone);
 }
 
+void sc_writeback_end(struct bio *clone)
+{
+	struct sc_io *sio = clone->bi_private;
+	struct sc_backing_dev *scbd = sio->scbd;
+	struct bio *bio = sio->front_bio;
+	sc_entry_t *entry;
+
+	sc_end_io_acct(sio);
+
+	bio->bi_status = clone->bi_status;
+	if (!bio->bi_status && op_is_write(bio_op(bio))) {
+		entry = sc_get_mapping_entry(scbd, bio->bi_iter.bi_sector);
+
+	}
+	bio_endio(bio);
+	free_sc_io(sio);
+}
+
+static bool sc_cache_clear(struct sc_cache_info *sci)
+{
+	unsigned long new, old;
+
+	old = READ_ONCE(sci->status);
+	while (1) {
+		if (SC_CACHE_INFO_INFLIGHT(old))
+			return false;
+
+		new = SC_CACHE_INFO(SC_CACHE_INFO_LAST(old),
+							SC_CACHE_INFO_AVG(old),
+							SC_CACHE_INFO_INFLIGHT(old),
+							0, 1);
+		old = cmpxchg(&sci->status, old, new);
+		if (old == new)
+			return true;
+	}
+}
+
+static bool sc_cache_invalidate(struct sc_cache_info *sci)
+{
+	unsigned long new, old;
+
+	old = READ_ONCE(sci->status);
+	while (1) {
+		/*
+		 * We can only invalidate clear cache block
+		 */
+		if (SC_CACHE_INFO_INFLIGHT(old) ||
+			SC_CACHE_INFO_DIRTY(old))
+			return false;
+
+		new = SC_CACHE_INFO(0, 0, 1, 0, 0);
+		old = cmpxchg(&sci->status, old, new);
+		if (old == new)
+			return true;
+	}
+}
+
+static unsigned long sc_calc_new(unsigned long old)
+{
+	unsigned int now, last, avg, inv, inflight;
+
+	now = jiffies;
+	last = SC_CACHE_INFO_LAST(old);
+	inv = now - last;
+	inflight = SC_CACHE_INFO_LAST(old);
+	avg = SC_CACHE_INFO_AVG(old);
+	avg *= 8;
+	avg += inv;
+	avg /= 8;
+	last = now;
+	inflight++;
+
+	return SC_CACHE_INFO(last, avg, inflight, 1, 1);
+}
+
+void sc_wait_invalidate(struct sc_backing_dev *scbd,
+		struct sc_cache_info *sci)
+{
+	wait_event(&scbd->wait_invalidate,
+			!SC_CACHE_INFO_INFLIGHT(READ_ONCE(sci->status)));
+}
+
+static bool sc_cache_access(struct sc_backing_dev *scbd,
+		struct sc_cache_info *sci)
+{
+	unsigned long old, new;
+
+	old = READ_ONCE(sci->status);
+	while (1) {
+		if (!SC_CACHE_INFO_VALID(old)) {
+			if (SC_CACHE_INFO_INFLIGHT(old)) {
+				/*
+				 * Before invalidate a cache block, the average of access interval
+				 * and last access time has been checked, so this should not be
+				 * a frequent case.
+				 */
+				pr_info("Accessing a cache block with being invalidated %lx\n",
+						sci->block);
+				sc_wait_invalidate(scbd, sci);
+			}
+			return false;
+		}
+
+		/*
+		 * dirty and valid bits are set
+		 */
+		new = sc_calc_new(old);
+		/*
+		 * May race with GC reclaim or concurrent submits and completions.
+		 */
+		old = cmpxchg(&sci->status, old, new);
+		/*
+		 * We wins.
+		 */
+		if (old == new)
+			return true;
+	}
+}
+
+static bool sc_cache_alloc(struct sc_backing_dev *scbd,
+		sc_entry_t *se, int block)
+{
+	/*
+	 * Update the se to indicate we are allocating, then
+	 * others would go to sleep to wait.
+	 *
+	 * So we need a bit that shows allocating.
+	 */
+}
+
+static blk_qc_t sc_writeback(struct sc_io *sio)
+{
+	struct sc_backing_dev *scbd = sio->scbd;
+	struct sc_sb *sb = scbd->sb;
+	struct bio *clone = &sio->clone;
+	struct bio *bio = sio->front_bio;
+	sector_t offset = bio->bi_iter.bi_sector;
+	sc_entry_t *e;
+
+	e = sc_get_mapping_entry(scbd, offset);
+	if (SC_CACHE_VALID(READ_ONCE(e->data))) {
+	
+		/*
+		 * Cache hit, write to cache directly
+		 */
+		__bio_clone_fast(clone, bio);
+		clone->bi_iter.bi_sector =
+			SC_CACHE_LBA(READ_ONCE(e->data), sb) + (offset - (offset & sb->bszmask));
+		bio_set_dev(clone, scbd->caching_dev);
+		//clone->bi_end_io = sio_writeback_end;
+		clone->bi_private = sio;
+		submit_bio(clone);
+	}
+	return BLK_QC_T_NONE;
+}
+
 static blk_qc_t sc_make_request(struct request_queue *q, struct bio *bio)
 {
 	struct sc_backing_dev *scbd = q->queuedata;
@@ -256,25 +415,467 @@ static const struct sysfs_ops sc_bd_sysfs_ops = {
 	.store	= sc_bd_attr_store,
 };
 
+void sc_block_wait(struct sc_backing_dev *scbd)
+{
+	DEFINE_WAIT(wait);
+
+	while (1) {
+		prepare_to_wait(&scbd->wait_block, &wait, TASK_UNINTERRUPTIBLE);
+		if (READ_ONCE(scbd->slot_available))
+			break;
+		io_schedule();
+	}
+	finish_wait(&scbd->wait_block, &wait);
+}
+/*
+ * This is a very special interface which can only be invoked
+ * by sc_setup_cache before writeabck mode starts. Nobody would
+ * race with us at the moment.
+ *
+ * TODO:
+ *  - noone races with use, so needn't to use atomic primitives.
+ */
+static void sc_block_set(struct sc_backing_dev *scbd, int block)
+{
+	int depth = scbd->slots[0].depth;
+	int i = block / depth;
+	unsigned long *map = scbd->slots[i].map;
+	int nr = block % depth;
+
+	set_bit(nr % BITS_PER_LONG, &map[nr / BITS_PER_LONG]);
+	if (!atomic_dec_return(&scbd->slots[i].available))
+		clear_bit(i, &scbd->slot_available);
+}
+
+/*
+ * TODO:
+ *  - allocate hint
+ *  - we need to try best to scatter different cpu to different slot
+ *  - make the depth is power2, then just need to shift
+ */
+static int sc_block_alloc(struct sc_backing_dev *scbd)
+{
+	int slot, nr, depth;
+	unsigned long *map;
+
+retry:
+	slot = find_next_zero_bit(&scbd->slot_available, BITS_PER_LONG, 0);
+	if (slot >= BITS_PER_LONG) {
+		sc_block_wait(scbd);
+		goto retry;
+	}
+
+	smp_mb__before_atomic();
+	if (!atomic_add_unless(&scbd->slots[slot].available, -1, 0))
+		goto retry;
+
+	/*
+	 * See comments in sc_block_free
+	 */
+	smp_mb__after_atomic();
+	map = scbd->slots[slot].map;
+	depth = scbd->slots[slot].depth;
+	while (1) {
+		/*
+		 * find_next_zero_bit support array, but test_and_set_bit not.
+		 */
+		nr = find_next_zero_bit(map, depth, 0);
+		BUG_ON(nr >= depth);
+
+		if (!test_and_set_bit(nr % BITS_PER_LONG,
+					&map[nr / BITS_PER_LONG]))
+			break;
+	}
+
+	return slot * depth + nr;
+}
+
+static void sc_block_free(struct sc_backing_dev *scbd, int block)
+{
+	int depth = scbd->slots[0].depth;
+	int i = block / depth;
+	unsigned long *map = scbd->slots[i].map;
+	int nr = block % depth;
+
+	clear_bit(nr % BITS_PER_LONG, &map[nr / BITS_PER_LONG]);
+	/*
+	 * Ensure the bit set in map is visible before we add the available
+	 */
+	smp_mb__before_atomic();
+	if (atomic_inc_return(&scbd->slots[i].available) == 1) {
+		/*
+		 * Ensure the slot available is visible before we set
+		 * the slot_available.
+		 */
+		smp_mb__before_atomic();
+		set_bit(i, &scbd->slot_available);
+	}
+
+	if (waitqueue_active(&scbd->wait_block))
+		wake_up(&scbd->wait_block);
+}
+
+static int sc_blocks_allocator_init(struct sc_backing_dev *scbd)
+{
+	int i;
+	struct sc_sb *sb = scbd->sb;
+	uint64_t depth, slot;
+	sector_t caching_size = part_nr_sects_read(scbd->caching_dev->bd_part);
+
+	depth = round_down((caching_size << 9), sb->block_size);
+	depth -= round_up(sb->mlen, sb->block_size);
+	depth /= sb->block_size;
+
+	scbd->scis = kzalloc(sizeof(struct sc_cache_info) * depth, GFP_KERNEL);
+	if (!scbd->scis) {
+		pr_err("Failed to allocate sc_cache_info array\n");
+		return -ENOMEM;
+	}
+	pr_info("available blocks in caching device %llu\n", depth);
+
+	slot = depth / SC_MAX_BITMAP_SLOTS;
+	for (i = 0; i < SC_MAX_BITMAP_SLOTS - 1; i++) {
+		scbd->slots[i].depth = slot;
+		atomic_set(&scbd->slots[i].available, slot);
+	}
+	pr_info("blocks per slot is %lld\n", slot);
+
+	slot = round_up(slot, BITS_PER_LONG);
+	slot /= BITS_PER_LONG;
+	for (i = 0; i < SC_MAX_BITMAP_SLOTS - 1; i++) {
+		scbd->slots[i].map = kzalloc(slot, GFP_KERNEL);
+		BUG_ON(!scbd->slots[i].map);
+	}
+	/*
+	 * Last slot initialization
+	 */
+	slot = depth % SC_MAX_BITMAP_SLOTS;
+	scbd->slots[SC_MAX_BITMAP_SLOTS - 1].depth = slot;
+	atomic_set(&scbd->slots[SC_MAX_BITMAP_SLOTS - 1].available, slot);
+	pr_info("blocks of last slot is %lld\n", slot);
+
+	slot = round_up(slot, BITS_PER_LONG);
+	slot /= BITS_PER_LONG;
+	scbd->slots[SC_MAX_BITMAP_SLOTS - 1].map = kzalloc(slot, GFP_KERNEL);
+	BUG_ON(!scbd->slots[SC_MAX_BITMAP_SLOTS - 1].map);
+
+	scbd->slot_available = 0;
+	init_waitqueue_head(&scbd->wait_block);
+	init_waitqueue_head(&scbd->wait_invalidate);
+	return 0;
+}
+
+static void sc_block_allocator_destroy(struct sc_backing_dev *scbd)
+{
+	int i;
+
+	for (i = 0; i < SC_MAX_BITMAP_SLOTS; i++)
+		kfree(scbd->slots[i].map);
+}
+
+static int sc_cache_format(struct sc_backing_dev *scbd,
+		struct page *page, struct bio *bio)
+{
+	struct block_device *cbdev = scbd->caching_dev;
+	struct sc_d_sb *dsb = page_address(page);
+	uint32_t block_size = scbd->setting.block_size;
+	sector_t backing_size = part_nr_sects_read(scbd->backing_dev->bd_part);
+	uint64_t mlen;
+	sector_t offset, end;
+	int ret;
+
+	mlen = round_up((backing_size << 9), block_size) / block_size;
+	mlen *= SC_CACHE_MAPPING_ENTRY_SIZE;
+	mlen += 512; /* superblock sector */
+	mlen = round_up(mlen, PAGE_SIZE); /* Just for convenience of read/write */
+
+	pr_info("backing capacity is %lu G Bytes mlen %lu M Bytes\n",
+			(unsigned long)(backing_size << 9) >> 30, (unsigned long)mlen >> 20);
+	pr_info("first available block is %llx\n", round_up(mlen, block_size) >> 9);
+
+	offset = 0;
+	end = mlen >> 9;
+
+	memset((void *)dsb, 0, PAGE_SIZE);
+	while (offset < end) {
+		bio_init(bio, bio->bi_inline_vecs, 1);
+		bio->bi_iter.bi_sector = offset;
+		bio_set_dev(bio, cbdev);
+		bio_set_op_attrs(bio, REQ_OP_WRITE, 0);
+		bio_add_page(bio, page, PAGE_SIZE, 0);
+		ret = submit_bio_wait(bio);
+		if (ret) {
+			pr_err("Failed to initialize the metadata %d\n", ret);
+			return ret;
+		}
+		offset += PAGE_SIZE;
+	}
+
+	dsb->sc_magic = cpu_to_le32(SC_MAGIC_NUMBER);
+	dsb->block_size = cpu_to_le32(block_size);
+	dsb->mlen = cpu_to_le64(mlen);
+
+	bio_init(bio, bio->bi_inline_vecs, 1);
+	bio->bi_iter.bi_sector = 0;
+	bio_set_dev(bio, cbdev);
+	bio_set_op_attrs(bio, REQ_OP_WRITE, 0);
+	bio_add_page(bio, page, 512, 0);
+	ret = submit_bio_wait(bio);
+	if (ret)
+		pr_err("Failed to write the superblock %d\n", ret);
+
+	return ret;
+}
+
+static int sc_get_sb(struct sc_backing_dev *scbd)
+{
+	struct block_device *cbdev = scbd->caching_dev;
+	struct bio *bio;
+	struct page *page;
+	struct sc_d_sb *dsb;
+	struct sc_sb *sb;
+	int ret = 0;
+
+	page = alloc_page(GFP_KERNEL);
+	if (!page)
+		return -ENOMEM;
+
+	bio = bio_alloc(GFP_KERNEL, 1);
+
+	bio_set_dev(bio, cbdev);
+	bio->bi_iter.bi_sector = 0;
+	bio_add_page(bio, page, PAGE_SIZE, 0);
+	bio_set_op_attrs(bio, REQ_OP_READ, 0);
+	ret = submit_bio_wait(bio);
+	if (ret) {
+		pr_err("Failed to read in sb %d\n", ret);
+		goto fail;
+	}
+
+	dsb = page_address(page);
+
+	if (le32_to_cpu(dsb->sc_magic) != SC_MAGIC_NUMBER) {
+		pr_warn("This is not scache caching device and "
+				"it will be formatted next.\n");
+		/*
+		 * The professional way should be done by userland
+		 * tool and this will be done if scache goes well.
+		 */
+		ret = sc_cache_format(scbd, page, bio);
+		if (ret)
+			goto fail;
+	}
+	/*
+	 * We need to check the checksum here.
+	 */
+	sb = kmalloc(sizeof(*sb), GFP_KERNEL);
+	if (!sb) {
+		pr_err("Failed to allocate sb\n");
+		goto fail;
+	}
+	sb->block_size = le32_to_cpu(dsb->block_size);
+	sb->bitsbsz2sc = ilog2(sb->block_size) - 9;
+	sb->bszmask = ~(sb->block_size - 1);
+	sb->mlen = le64_to_cpu(dsb->mlen);
+	sb->first_page = page;
+	scbd->sb = sb;
+
+	bio_put(bio);
+	return 0;
+fail:
+	bio_put(bio);
+	put_page(page);
+	return ret;
+}
+
+static void sc_put_sb(struct sc_backing_dev *scbd)
+{
+	struct sc_sb *sb = scbd->sb;
+
+	put_page(sb->first_page);
+	kfree(sb);
+}
+
+static sc_entry_t *sc_get_mapping_entry(struct sc_backing_dev *scbd, sector_t sec)
+{
+	struct sc_sb *sb = scbd->sb;
+	int block = sec >> sb->bitsbsz2sc;
+
+	block += SC_MAPPING_FIRST_PAGE_OFFSET;
+
+	return &sb->mappings[block / SC_ENTRIES_PER_PAGE][block % SC_ENTRIES_PER_PAGE];
+}
+
+struct sc_setup_mapping_data {
+	atomic_t bios;
+	wait_queue_head_t waitq;
+};
+
+static void sc_read_mapping_endio(struct bio *bio)
+{
+	struct sc_setup_mapping_data *data = bio->bi_private;
+
+	if (!atomic_dec_return(&data->bios))
+		wake_up(&data->waitq);
+}
+
+static int sc_read_mapping(struct sc_backing_dev *scbd)
+{
+	struct sc_sb *sb = scbd->sb;
+	struct bio *bio = NULL;
+	sector_t offset;
+	int i, len;
+	struct sc_setup_mapping_data data;
+	struct blk_plug plug;
+
+	atomic_set(&data.bios, 0);
+	init_waitqueue_head(&data.waitq);
+	/*
+	 * First page has been read in.
+	 */
+	offset = PAGE_SECTORS;
+	len = sb->mlen / PAGE_SIZE - 1;
+	i = 1;
+	blk_start_plug(&plug);
+	while (len) {
+		if (!bio) {
+			bio = bio_alloc(GFP_KERNEL, min_t(int, len, BIO_MAX_PAGES));
+			bio_set_dev(bio, scbd->caching_dev);
+			bio->bi_iter.bi_sector = offset;
+			bio_set_op_attrs(bio, REQ_OP_READ, 0);
+			bio->bi_end_io = sc_read_mapping_endio;
+			bio->bi_private = &data;
+			atomic_inc(&data.bios);
+		}
+		if (!bio_add_page(bio, sb->map_pages[i], PAGE_SIZE, 0)) {
+			submit_bio(bio);
+			bio = NULL;
+			continue;
+		}
+		offset += PAGE_SECTORS;
+		len -= 1;
+		i += 1;
+	}
+	if (bio)
+		submit_bio(bio);
+	blk_finish_plug(&plug);
+	wait_event(data.waitq, !atomic_read(&data.bios));
+
+	return 0;
+}
+
+static int sc_handle_mapping_early(struct sc_backing_dev *scbd)
+{
+	struct sc_sb *sb = scbd->sb;
+	int i, len = sb->mlen / PAGE_SIZE;
+	sc_entry_t *start, *end;
+	struct sc_cache_info *sci;
+	unsigned long cblock, bblock;
+
+	/*
+	 * Pick the used blocks out of block allocator.
+	 */
+	bblock = 0;
+	for (i = 0; i < len; i++) {
+		start = sb->mappings[i];
+		end = start + SC_ENTRIES_PER_PAGE;
+		if (i == 0)
+			start += SC_MAPPING_FIRST_PAGE_OFFSET;
+		for (; start < end; start++) {
+			bblock++;
+			if (SC_CACHE_VALID(READ_ONCE(start->data))) {
+				cblock = SC_CACHE_BLOCK(READ_ONCE(start->data));
+				sc_block_set(scbd, cblock);
+				sci = &scbd->scis[cblock];
+				sci->status = 1; /* valid entry */
+				sci->block = bblock; /* reverse mapping */
+			}
+		}
+	}
+
+	return 0;
+}
+
+static int sc_setup_mapping(struct sc_backing_dev *scbd)
+{
+	struct sc_sb *sb = scbd->sb;
+	int i, ret, len = sb->mlen / PAGE_SIZE;
+
+
+	sb->map_pages = kzalloc(sizeof(void *) * len, GFP_KERNEL);
+	if (!sb->map_pages) {
+		pr_err("Failed to allocate map_pages\n");
+		return -ENOMEM;
+	}
+	sb->mappings = kzalloc(sizeof(void *) * len, GFP_KERNEL);
+	if (!sb->mappings) {
+		pr_err("Failed to allocate mappings\n");
+		ret = -ENOMEM;
+		goto fail_mappings;
+	}
+
+	sb->map_pages[0] = sb->first_page;
+	sb->mappings[0] = page_address(sb->first_page);
+	for (i = 1; i < len; i++) {
+		sb->map_pages[i] = alloc_page(GFP_KERNEL);
+		if (!sb->map_pages[i]) {
+			pr_err("Failed to allocate map pages\n");
+			for (; i > 0; i--)
+				put_page(sb->map_pages[i]);
+			ret = -ENOMEM;
+			goto fail_pages;
+		}
+		sb->mappings[i] = page_address(sb->map_pages[i]);
+	}
+
+	ret = sc_read_mapping(scbd);
+
+	ret = sc_handle_mapping_early(scbd);
+
+fail_pages:
+	kfree(sb->mappings);
+fail_mappings:
+	kfree(sb->map_pages);
+	return ret;
+}
+
+static void sc_destroy_mapping(struct sc_backing_dev *scbd)
+{
+	struct sc_sb *sb = scbd->sb;
+	int i, len = sb->mlen / PAGE_SIZE;
+	/*
+	 * The dirty mapping should have been written out to disk
+	 * First page is freed by sc_put_sb
+	 */
+	for (i = 1; i < len; i++)
+		put_page(sb->map_pages[i]);
+	kfree(sb->mappings);
+	kfree(sb->map_pages);
+}
+
 static int sc_setup_cache(struct sc_backing_dev *scbd)
 {
-	/*
-	 * Read in the super block of the caching_dev (1st sector ?)
-	 * If not there, setup a new one here.
-	 */
+	int ret;
 
-	/*
-	 * Read in the mapping
-	 */
+	ret = sc_get_sb(scbd);
+	if (ret)
+		goto out;
 
-	/*
-	 * Pick up the busy blocks and setup a freespace list
-	 */
+	ret = sc_blocks_allocator_init(scbd);
+	if (ret)
+		goto fail_ba;
 
+	ret = sc_setup_mapping(scbd);
 	/*
 	 * writeabck is ready
 	 */
-	return 0;
+
+	return ret;
+
+fail_ba:
+	sc_put_sb(scbd);
+out:
+	return ret;
 }
 
 static ssize_t sc_bd_cache_store(struct sc_backing_dev *scbd,
@@ -310,8 +911,13 @@ static ssize_t sc_bd_cache_store(struct sc_backing_dev *scbd,
 		goto fail_link_holder;
 	}
 	err = sc_setup_cache(scbd);
-	if (!err)
-		goto fail;
+	if (!err) {
+		/*
+		 * This is the successful path
+		 */
+		err = len;
+		goto fail_bdev;
+	}
 
 	bd_unlink_disk_holder(bdev, scbd->front_disk);
 fail_link_holder:
@@ -326,17 +932,30 @@ static void sc_turndown_cache(struct sc_backing_dev *scbd)
 {
 	/*
 	 * Flush dirty metadata into disk
+	 */
+	/*
 	 * Free mapping data
+	 */
+	sc_destroy_mapping(scbd);
+	/*
 	 * Free writeabck data
+	 */
+	/*
 	 * Free freespace managment data
 	 */
+	sc_block_allocator_destroy(scbd);
 }
 
 static ssize_t sc_bd_stop_store(struct sc_backing_dev *scbd,
 		const char *buf, size_t len)
 {
+	if (!READ_ONCE(scbd->caching_dev)) {
+		pr_warn("Caching device has been detached\n");
+		return -EINVAL;
+	}
 	/*
-	 * Stop new IO on front disk
+	 * Start write through mod
+	 * then we could delete the caching device transparently
 	 */
 
 	/*
@@ -344,11 +963,15 @@ static ssize_t sc_bd_stop_store(struct sc_backing_dev *scbd,
 	 */
 
 	sc_turndown_cache(scbd);
+
+	sc_put_sb(scbd);
+
+	bd_unlink_disk_holder(scbd->caching_dev, scbd->front_disk);
 	blkdev_put(scbd->caching_dev, FMODE_READ|FMODE_WRITE|FMODE_EXCL);
 	scbd->caching_dev = NULL;
 
 	/*
-	 * Start write through mod
+	 * Need some sync method against the hot path
 	 */
 	return len;
 }
@@ -410,6 +1033,7 @@ static int sc_backing_init(struct sc_backing_dev *scbd)
 	mutex_init(&scbd->sysfs_lock);
 	scbd->mode = SC_CACHE_PASSTHROUGH;
 	init_waitqueue_head(&scbd->wait_compl);
+	scbd->setting.block_size = SC_DEFAULT_CACHE_BLOCK_SIZE;
 
 	scbd->inflight_io = alloc_percpu(local_t);
 	if (!scbd->inflight_io) {
--- drivers/md/simple_cache.h
+++ drivers/md/simple_cache.h
@@ -3,6 +3,7 @@
 
 #define SC_MINORS 16 /* partition support */
 #define SC_IO_POOL_MAX 64
+#define PAGE_SECTORS (PAGE_SIZE / 512)
 
 #define __INIT_KOBJ_ATTR(_name, _mode, _show, _store)			\
 {									\
@@ -31,9 +32,87 @@ enum sc_cache_mode {
 	SC_CACHE_WRITEBACK,
 };
 
+/*
+ * (Assume machine is 64bit. I have to do this because I need
+ * more information in 'status' field)
+ *
+ * 63    48 47   32 31           2 1 0
+ * |-------|-------|-------------|-|-|
+ *
+ * 63:48    last access jiffies
+ * 47:32    average access interval
+ * 31:2     in-flight IO
+ * 1        dirty/clear
+ * 0        valid/invalid bit
+ *
+ * The max last access jiffies is 2^16 (32K), for a machine with HZ=1000,
+ * it is about 32s. This is enougth, because a cache block must have been
+ * invalidated before this. (Our GC period is less than 30s)
+ */
+struct sc_cache_info {
+	unsigned long status;
+	unsigned long block; /* block of backing device */
+};
+
+#define SC_CACHE_INFO_VALID(i) (i & 0x1)
+#define SC_CACHE_INFO_DIRTY(i) (i & 0x2)
+#define SC_CACHE_INFO_INFLIGHT(i) ((i & 0xffffffff) >> 2)
+#define SC_CACHE_INFO_LAST(i) (i >> 48)
+#define SC_CACHE_INFO_AVG(i) ((i >> 32) & 0xffff)
+#define SC_CACHE_INFO(l, a, f, d, i) \
+	((l << 48) | ((a << 32) & 0xffff) | \
+	 ((f & 0xffffffff) << 2) | \
+	 (d << 1) | i)
+
+/*
+ * Process of reclaim a cache block
+ *        clear dirty
+ *             |
+ *             v
+ *   write cache out to backing
+ *             |
+ *             v
+ *         invalidate     (invalid & inflight io)
+ *             |              \
+ *             v              |
+ *    update mapping to disk   > new update to the same entry will be blocked
+ *             |              |
+ *             v              /
+ *       free cache block
+ */
+
+#define SC_MAX_BITMAP_SLOTS (BITS_PER_LONG)
+
+/*
+ * bit value
+ *     0        available
+ *     1        used
+ */
+struct sc_bitmap_slot {
+	unsigned long *map;
+	int hint;
+	int depth;
+	atomic_t available;
+};
+
+/*
+ * Setting that could be changed by sysfs
+ */
+struct sc_cache_setting {
+	uint32_t 	block_size;
+};
+
 struct sc_backing_dev {
 	struct block_device *backing_dev;
 	struct block_device *caching_dev;
+
+	struct sc_sb *sb; /* superblock on caching device */
+
+	unsigned long slot_available;
+	struct sc_bitmap_slot slots[SC_MAX_BITMAP_SLOTS];
+	wait_queue_head_t wait_block;
+	struct sc_cache_info *scis; /* sc_cache_info array */
+	wait_queue_head_t wait_invalidate;
 
 	struct gendisk *front_disk;
 	struct request_queue *front_queue;
@@ -48,6 +127,8 @@ struct sc_backing_dev {
 
 	local_t __percpu *inflight_io;
 	wait_queue_head_t wait_compl; /* for draining in-flight IO */
+
+	struct sc_cache_setting setting;
 };
 
 struct sc_io {
@@ -61,6 +142,103 @@ struct sc_sysfs_entry {
 	struct attribute attr;
 	ssize_t (*show)(struct sc_backing_dev *, char *);
 	ssize_t (*store)(struct sc_backing_dev *, const char *, size_t);
+};
+
+/*
+ * caching and backing block mapping
+ *
+ * Most capacity of caching device is 1T (2^40)
+ * Smallest block size is 64K (2^16)
+ *
+ * So we need 24 bits per entry at most for caching device lba
+ * 
+ * 31                8 7             0
+ * |-------------------|--------------|
+ * \________ _________/ 
+ *          V
+ *     caching lba
+ *
+ * bit 31 ~ 8 	cache lba in block_size 
+ * bit 7 ~ 0 	entry flags
+ *
+ * bit 0 	valid/invalid
+ * bit 1 	dirty/clean
+ *
+ * others 	reserved
+ *
+ * Metadata size = (backing_capacity/block_size) * 4 bytes
+ * 
+ * Backing capacity    Block size    Metadata size
+ *              1T           512K    8M
+ *              1T           256K    16M
+ *              1T           128K    32M
+ *              1T            64K    64M
+ * Metadata is reserved after the super block sector as following.
+ *
+ *             +--------+ superblock sector (1 sector)
+ *             +--------+
+ *             |        | mapping
+ *             |        |
+ *             +--------+ padding for block size alignement
+ *             +--------+
+ *             |        |
+ *             |        |
+ *             .        .
+ *             .        . available space for caching
+ *             .        .
+ *             |        |
+ *             +--------+
+ *
+ *
+ */
+
+#define SC_DEFAULT_CACHE_BLOCK_SIZE 131072 /* 128K Bytes */
+#define SC_CACHE_MAPPING_ENTRY_SIZE 4 /* Bytes */
+#define SC_ENTRIES_PER_PAGE (PAGE_SIZE / SC_CACHE_MAPPING_ENTRY_SIZE)
+#define SC_MAPPING_FIRST_PAGE_OFFSET (512 / SC_CACHE_MAPPING_ENTRY_SIZE)
+
+struct sc_entry {
+	uint32_t data;
+};
+
+typedef struct sc_entry sc_entry_t;
+
+#define SC_CACHE_FLAGS_SHIFT 8
+#define SC_CACHE_FLAGS_VALID_SHIFT 0
+#define SC_CACHE_FLAGS_DIRTY_SHIFT 1
+
+
+#define SC_CACHE_VALID(e) (e & (1 << SC_CACHE_FLAGS_VALID_SHIFT))
+#define SC_CACHE_DIRTY(e) (e & (1 << SC_CACHE_FLAGS_DIRTY_SHIFT))
+
+#define SC_CACHE_BLOCK(e) (e >> SC_CACHE_FLAGS_SHIFT)
+#define SC_CACHE_LBA(e, sb) ((e >> SC_CACHE_FLAGS_SHIFT) << (sb->bitsbsz2sc))
+
+
+#define SC_MAGIC_NUMBER 0x53435342
+/*
+ * In-core scache super block
+ */
+struct sc_sb {
+	uint32_t 	block_size;
+	uint8_t 	bitsbsz2sc; /* bits need to shift to convert
+				       from block_size to sector or reverse */
+	sector_t 	bszmask;
+	uint64_t 	mlen; /* Bytes, length of all metadata, sb and mapping,
+				 block_size aligned */
+	struct page 	*first_page; /* super block is contained in it */
+	struct page 	**map_pages; /* include the first page */
+	sc_entry_t **mappings;
+};
+
+/*
+ * On-disk scache super block which locates on 1st sector of caching device
+ */
+struct sc_d_sb {
+	__u32 		sc_magic; /* SC_MAGIC_NUMBER */
+	__u64 		csum;
+	__u32 		block_size; /* Bytes */
+	__u64 		mlen; /* Bytes, length of all metadata */
 };
 
 #endif
